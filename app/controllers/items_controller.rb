386947d011d023ff2bb6a5debabedb55dc23470f
hermione415 <rippy.t.310@gmail.com>
2021-10-30 16:35:24 +0900

    商品削除機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
       redirect_to root_path
     else
       render :show
